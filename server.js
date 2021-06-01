d4b85f59f359cc7fc8c45e8ea098405c85245666
abhalondhe <84110592+abhalondhe@users.noreply.github.com>
2021-06-01 01:36:50 -0400

    Update server.js

--- server.js
+++ server.js
@@ -161,13 +161,14 @@ var userPapers="";
 //check s3 location
  const params = {
         Bucket: 'chaanakya',
+	Key: 'kbre/user_ans/*',
   	MaxKeys: 200
  };
  s3.listObjectsV2(params, function(err, data) {
    if(err){console.log(err, err.stack); // an error occurred
    }
    else{
-	   console.log(data); 
+	   console.log(data.Contents); 
 	   userPapers = data;
    }
  });
